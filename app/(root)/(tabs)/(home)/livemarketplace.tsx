f6eeb685b0fc31b6d1ec6f25cea47df2c1cf865c
Ifure <ifureudoh08@gmail.com>
2024-12-24 14:20:26 +0100

    font setup

---
+++ app/(root)/(tabs)/(home)/livemarketplace.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const livemarketplace = () => {
+  return (
+    <View>
+      <Text>livemarketplace</Text>
+    </View>
+  )
+}
+
+export default livemarketplace
